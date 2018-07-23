8e8ee57b289edea178b3c3610b6932cc2948dc01
Staszek Zawadzki <s.zawadzki@visuality.pl>
2018-07-23 17:28:17 +0200

    Basic layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,16 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def about
+  end
+
+  def next
+  end
+
+  def calc
+  end
+
+  def organizer
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   get 'home/index'
+  get 'home/about'
+  get 'home/next'
+  get 'home/calc'
+  get 'home/organizer'
   root 'home#index'
 end
